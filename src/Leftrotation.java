9c708b99a73c150f85ced50f8526e92cc971e3a6
allpurecode <rishika212110@gmail.com>
2024-09-27 12:09:44 +0530

    27/09/2024

--- src/Leftrotation.java
+++ src/Leftrotation.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Leftrotation {
+/*public class Leftrotation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[5];
@@ -20,4 +20,39 @@ public class Leftrotation {
 
 
     }
+    }*/
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+class Solution {
+    public int romanToInt(String s) {
+        Map <Character,Integer> romanVal=new HashMap<>();
+        romanVal.put('I', 1);
+        romanVal.put('V', 5);
+        romanVal.put('X', 10);
+        romanVal.put('L', 50);
+        romanVal.put('C', 100);
+        romanVal.put('D', 500);
+        romanVal.put('M', 1000);
+
+        int  result=0;
+        for(int i=s.length()-1;i>=0;i--){
+            int currValue = romanVal.get(s.charAt(i));
+
+            if (i < s.length() - 1 && currValue < romanVal.get(s.charAt(i + 1))) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+        }
+
+        return result;
     }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String s= sc.nextLine();
+        Solution obj=new Solution();
+        System.out.println(obj.romanToInt(s));
+    }
+}
+
