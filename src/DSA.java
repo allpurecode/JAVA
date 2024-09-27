20204f61894b2a4fbbb9b5963ffbf8d2a051eefd
allpurecode <rishika212110@gmail.com>
2024-09-27 09:50:52 +0530

    26/09/2024

--- src/DSA.java
+++ src/DSA.java
@@ -551,9 +551,77 @@ class DSA {
 
     }
 }*/
+//------------------------------------------------------------------------------------------------------------------------------
+// fibonacci search
+import java.util.*;
+
+class DSA {
+    public static int fibonacciSearch(int[] arr, int x) {
+        int n = arr.length;
+        if (n == 0) {
+            return -1;
+        }
+
+        // Initialize Fibonacci numbers
+        int fib1 = 0, fib2 = 1, fib3 = fib1 + fib2;
+
+        // Find the smallest Fibonacci number greater than or equal to n
+        while (fib3 < n) {
+            fib1 = fib2;
+            fib2 = fib3;
+            fib3 = fib1 + fib2;
+        }
+
+        // Initialize variables for the current and previous split points
+        int offset = -1;
+        while (fib3 > 1) {
+            int i = Math.min(offset + fib2, n-1);
+
+            // If x is greater than the value at index i, move the split point to the right
+            if (arr[i] < x) {
+                fib3 = fib2;
+                fib2 = fib1;
+                fib1 = fib3 - fib2;
+                offset = i;
+            }
+
+            // If x is less than the value at index i, move the split point to the left
+            else if (arr[i] > x) {
+                fib3 = fib1;
+                fib2 = fib2 - fib1;
+                fib1 = fib3 - fib2;
+            }
+
+            // If x is equal to the value at index i, return the index
+            else {
+                return i;
+            }
+        }
+
+        // If x is not found in the array, return -1
+        if (fib2 == 1 && arr[offset+1] == x) {
+            return offset + 1;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100, 235};
+        int n = arr.length;
+        int x = 235;
+        int ind = fibonacciSearch(arr, x);
+        if (ind >= 0) {
+            System.out.println("Found at index: " + ind);
+        } else {
+            System.out.println(x + " isn't present in the array");
+        }
+    }
+}
+
 // week 4-----------------------------------------------------------------------------------------------------------------------------
 //quick sort
-import java.util.Stack;
+/*import java.util.Stack;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -612,4 +680,4 @@ public class DSA {
         quickSortIterative(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
-}
+}*/
