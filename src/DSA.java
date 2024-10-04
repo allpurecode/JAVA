a966d6995e799f61e13040dca48799948bb7c198
allpurecode <rishika212110@gmail.com>
2024-10-04 11:07:55 +0530

    04/10/2024

--- src/DSA.java
+++ src/DSA.java
@@ -1069,7 +1069,7 @@ class StackExample {
 }
 */
 // BLANCED PARENTHESE---------------------------------------------------------------------------------------------------------
-import java.util.Stack;
+/*import java.util.Stack;
 
 class BalancedParenthesisChecker {
     // Method to check if parentheses are balanced
@@ -1110,6 +1110,61 @@ class BalancedParenthesisChecker {
         System.out.println(expression1 + " : " + isBalanced(expression1));  // true
         System.out.println(expression2 + " : " + isBalanced(expression2));  // false
     }
-}
+}*/
+// EVALUATION OF POSTFIX EXPRESSION ---------------------------------------------------------------------------------------------------------
+import java.util.Stack;
+import java.util.Stack;
 
+public class DSA {
+
+    // Method to evaluate a postfix expression
+    public static int evaluatePostfix(String expression) {
+        // Create a stack to store operands
+        Stack<Integer> stack = new Stack<>();
+
+        // Traverse through every character in the postfix expression
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+
+            // If the character is a digit, push it onto the stack
+            if (Character.isDigit(c)) {
+                stack.push(c - '0'); // Convert char to int
+            }
+            // Otherwise, the character is an operator
+            else {
+                // Pop two operands from the stack
+                int operand2 = stack.pop();
+                int operand1 = stack.pop();
+
+                // Perform the operation and push the result back to the stack
+                switch (c) {
+                    case '+':
+                        stack.push(operand1 + operand2);
+                        break;
+                    case '-':
+                        stack.push(operand1 - operand2);
+                        break;
+                    case '*':
+                        stack.push(operand1 * operand2);
+                        break;
+                    case '/':
+                        stack.push(operand1 / operand2);
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid operator: " + c);
+                }
+            }
+        }
+
+        // The result is the last element remaining in the stack
+        return stack.pop();
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        String postfix = "53+62/*35*+";
+        int result = evaluatePostfix(postfix);
+        System.out.println("The result of the postfix expression is: " + result);
+    }
+}
 
