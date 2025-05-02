242bcce54d0c17b2ed6f176c54fb74e8e22d0ed0
Your Name <your.email@example.com>
2025-05-02 11:06:11 +0530

    02/05/25

--- src/Basics.java
+++ src/Basics.java
@@ -73,7 +73,7 @@ import java.util.Scanner;
 }
 */
 // Second largest_one pass-------------------------------------------------------------------------------------------------------------------------------------------
-class Basics{
+/*class Basics{
     public int getSecondLast_onepass(int arr[]){
         int n=arr.length;
         int largest=-1,secondLargest=-1;
@@ -106,4 +106,105 @@ class Basics{
         Basics obj = new Basics();
         System.out.println(obj.getSecondLast_onepass(arr1));
     }
+}
+
+ */
+ // third largest element _basic-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*class Basics{
+    public int Third_largest_element(int arr[]){
+        Arrays.sort(arr);
+        int n=arr.length;
+        return arr[n-3];
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int arr1[] = new int[size];
+        System.out.println("Enter " + size + " elements: ");
+        for (int i = 0; i < size; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        Basics obj = new Basics();
+        System.out.println(obj.Third_largest_element(arr1));
+    }
+
+}
+
+ */
+// Third Largest three loops -------------------------------------------------------------------------------------------------------
+
+/*class Basics{
+    public int ThirdLargest_three_loops(int arr[]){
+        int n=arr.length;
+        int first=-1;
+        int second =-1;
+        int third =-1;
+
+        for(int i=0;i<n;i++){
+            if(arr[i]>first) first=arr[i];
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > second && arr[i] < first) {
+                second = arr[i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > third && arr[i] < second) {
+                third = arr[i];
+            }
+        }
+        return third;
+
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int arr1[] = new int[size];
+        System.out.println("Enter " + size + " elements: ");
+        for (int i = 0; i < size; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        Basics obj = new Basics();
+        System.out.println(obj.ThirdLargest_three_loops(arr1));
+    }
+
+}
+
+ */
+// third_largest element using one loop -----------------------------------------------------------------------------------------
+ class Basics{
+     static int thirdlargest_oneloop(int arr[]){   // if static key word is used there is no need of creation of object
+         int first=-1;
+         int second=-1;
+         int third=-1;
+         int n=arr.length;
+         for(int i=0;i<n;i++){
+             if(arr[i]>first){
+                  third=second;
+                  second=first;
+                  first=arr[i];
+
+
+             } else if (arr[i]>second) {
+                 third=second;
+                 second=arr[i];
+             } else if (arr[i]>third) {
+                 third=arr[i];
+
+             }
+
+         }
+         return third;
+     }
+    public static void main(String[] args) {
+        int[] arr = {1, 14, 2, 16, 10, 20};
+        System.out.println(thirdlargest_oneloop(arr));
+    }
+
 }
